aa25751fbddf103c6470586cddd0e46da7f063d9
Shrey Kumar Gupta <shreykr.gupta1260@gmail.com>
2026-08-13 16:09:55 +0530

    oop_auto_keyword_added

---
+++ CHAPTER-1/auto_keyword.cpp
@@ -0,0 +1,18 @@
+//auto keyword
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    auto a = 5;
+    auto b = 0.5;
+    auto c = 'A';
+    auto d = -0.16;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+
+    return 0;
+}
